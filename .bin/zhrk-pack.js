dfe4bf30ad05ede32bf9591bdf59a7daec500ec4
zhrk <zshchipets@gmail.com>
2024-04-04 18:24:45 +0400

    fix bin

--- .bin/zhrk-pack.js
+++ .bin/zhrk-pack.js
@@ -1,29 +1,36 @@
 #!/usr/bin/env node
+const { spawnSync } = require("child_process");
 
-const { spawn } = require("child_process");
+// chalk для ошибки несуществуюего скрипта
+// обработать занятый порт
+// вынести тесты
 
-const scriptName = process.argv[2];
+const script = process.argv[2];
 
 const scripts = {
-  start: "npm run start",
-  build: "npm run build",
+  start: { command: "webpack", args: ["serve", "--mode", "development"] },
+  build: { command: "webpack", args: ["--mode", "production"] },
 };
 
-if (!scripts.hasOwnProperty(scriptName)) {
-  console.error(`Invalid script: ${scriptName}`);
+const { command, args } = scripts[script];
+
+if (!scripts.hasOwnProperty(script)) {
+  console.error(`Script ${script} does not exist `);
+
   process.exit(1);
 }
 
-const child = spawn(scripts[scriptName], {
-  stdio: "inherit",
+const child = spawnSync(command, args, {
   shell: true,
+  stdio: "inherit",
 });
 
-child.on("exit", (code, signal) => {
-  if (code !== null) {
-    process.exit(code);
-  }
-  if (signal !== null) {
-    process.kill(process.pid, signal);
-  }
-});
+// child.on('exit', (code, signal) => {
+//   if (code !== null) {
+//     process.exit(code);
+//   }
+
+//   if (signal !== null) {
+//     process.kill(process.pid, signal);
+//   }
+// });
