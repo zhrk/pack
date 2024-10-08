81969d4ec4c06314f1dd244833d164468828fa41
zhrk <zshchipets@gmail.com>
2024-10-08 23:16:03 +0400

    added ability use CLI arguments

--- scripts.js
+++ scripts.js
@@ -13,17 +13,20 @@ const webpackConfigPath = path.join(__dirname, './webpack.config.js');
 const rsbuildConfigPath = path.join(__dirname, './rsbuild.config.ts');
 const vitestConfigPath = path.join(__dirname, './vitest.config.ts');
 
-const script = process.argv[2];
+const [, , script, ...rest] = process.argv;
 
 const scripts = {
   start: {
     command: 'webpack',
-    args: ['serve', '--mode', 'development', '--config', webpackConfigPath],
+    args: ['serve', '--mode', 'development', '--config', webpackConfigPath, ...rest],
   },
-  build: { command: 'webpack', args: ['--mode', 'production', '--config', webpackConfigPath] },
-  'start:rsbuild': { command: 'rsbuild', args: ['dev', '--config', rsbuildConfigPath] },
-  'build:rsbuild': { command: 'rsbuild', args: ['build', '--config', rsbuildConfigPath] },
-  test: { command: 'vitest', args: ['--config', vitestConfigPath] },
+  build: {
+    command: 'webpack',
+    args: ['--mode', 'production', '--config', webpackConfigPath, ...rest],
+  },
+  'start:rsbuild': { command: 'rsbuild', args: ['dev', '--config', rsbuildConfigPath, ...rest] },
+  'build:rsbuild': { command: 'rsbuild', args: ['build', '--config', rsbuildConfigPath, ...rest] },
+  test: { command: 'vitest', args: ['--config', vitestConfigPath, ...rest] },
 };
 
 const { command, args } = scripts[script];
