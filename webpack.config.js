3e407ee5d84c335ca52429375e78ef96d3b1f7e2
zhrk <zshchipets@gmail.com>
2024-10-10 15:27:20 +0400

    changed dev server logging level

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = (_, argv) => {
       compress: false,
       historyApiFallback: true,
       static: { publicPath: '/' },
-      client: { logging: 'error' },
+      client: { logging: 'none' },
     },
     module: {
       rules: [
