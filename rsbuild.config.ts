16a26c64dba24ceae235f7e8ff5fe64b00a85acb
Zakhar Shchipets <zshchipets@gmail.com>
2025-01-09 15:24:36 +0400

    Update rsbuild.config.ts

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -6,6 +6,9 @@ import { pluginSvgr } from '@rsbuild/plugin-svgr';
 import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
 
 // отображение ошибок eslint, typescript
+// https://github.com/web-infra-dev/rsbuild/issues/4232
+// https://github.com/rspack-contrib/rsbuild-plugin-type-check
+// https://github.com/rspack-contrib/rsbuild-plugin-type-check/releases/tag/v1.2.0
 
 export default defineConfig({
   plugins: [
