f7aacb224ac7b2458c075cab07d3f4e1e3a048dd
zhrk <zshchipets@gmail.com>
2025-03-19 17:27:59 +0400

    feat(rsbuild): remove comments & auto open dev server

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -5,11 +5,6 @@ import { pluginSass } from '@rsbuild/plugin-sass';
 import { pluginSvgr } from '@rsbuild/plugin-svgr';
 import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
 
-// отображение ошибок eslint, typescript
-// https://github.com/web-infra-dev/rsbuild/issues/4232
-// https://github.com/rspack-contrib/rsbuild-plugin-type-check
-// https://github.com/rspack-contrib/rsbuild-plugin-type-check/releases/tag/v1.2.0
-
 export default defineConfig({
   plugins: [
     pluginSass(),
@@ -18,6 +13,7 @@ export default defineConfig({
     pluginTypeCheck({ forkTsCheckerOptions: { typescript: { memoryLimit: 8192 } } }),
     pluginSvgr({ svgrOptions: { ref: true, icon: true, exportType: 'default' } }),
   ],
+  server: { open: true },
   html: { template: './public/index.html' },
   output: {
     distPath: { root: 'build' },
