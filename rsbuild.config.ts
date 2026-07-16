b4fc22bf276425bf21d19978c4039052fe740cad
zhrk <zshchipets@gmail.com>
2026-07-16 11:38:44 +0400

    color-opacity safari 17.0 fix

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
         require('postcss-functions')({
           functions: {
             'color-opacity': (color: string, opacity: string) =>
-              `rgb(from ${color} r g b / ${parseFloat(opacity) * 100}%)`,
+              `color-mix(in srgb, ${color}, transparent ${100 - parseFloat(opacity) * 100}%)`,
           },
         })
       );
