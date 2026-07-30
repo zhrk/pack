ffba95cdf0c50c1df6d04f664ae8d1a6bf86b09f
zhrk <zshchipets@gmail.com>
2026-07-30 16:25:26 +0400

    remove eslint jsx-a11y

--- eslint.config.js
+++ eslint.config.js
@@ -267,67 +267,6 @@ module.exports = [
         },
       ],
 
-      // jsx-a11y
-      'jsx-a11y/alt-text': [
-        'error',
-        { elements: ['img', 'object', 'area', 'input[type="image"]'] },
-      ],
-      'jsx-a11y/anchor-has-content': ['error'],
-      'jsx-a11y/anchor-is-valid': [
-        'error',
-        {
-          components: ['Link'],
-          specialLink: ['to'],
-          aspects: ['noHref', 'invalidHref', 'preferButton'],
-        },
-      ],
-      'jsx-a11y/aria-activedescendant-has-tabindex': ['error'],
-      'jsx-a11y/aria-props': ['error'],
-      'jsx-a11y/aria-proptypes': ['error'],
-      'jsx-a11y/aria-role': ['error'],
-      'jsx-a11y/aria-unsupported-elements': ['error'],
-      'jsx-a11y/autocomplete-valid': ['error'],
-      // 'jsx-a11y/click-events-have-key-events': ['error'],
-      'jsx-a11y/heading-has-content': ['error'],
-      'jsx-a11y/html-has-lang': ['error'],
-      'jsx-a11y/iframe-has-title': ['error'],
-      'jsx-a11y/img-redundant-alt': ['error'],
-      'jsx-a11y/interactive-supports-focus': ['error'],
-      'jsx-a11y/lang': ['error'],
-      'jsx-a11y/mouse-events-have-key-events': ['error'],
-      'jsx-a11y/no-access-key': ['error'],
-      'jsx-a11y/no-aria-hidden-on-focusable': ['error'],
-      'jsx-a11y/no-distracting-elements': ['error', { elements: ['marquee', 'blink'] }],
-      'jsx-a11y/no-interactive-element-to-noninteractive-role': [
-        'error',
-        { tr: ['none', 'presentation'] },
-      ],
-      'jsx-a11y/no-noninteractive-element-interactions': [
-        'error',
-        { handlers: ['onClick', 'onMouseDown', 'onMouseUp', 'onKeyPress', 'onKeyDown', 'onKeyUp'] },
-      ],
-      'jsx-a11y/no-noninteractive-element-to-interactive-role': [
-        'error',
-        {
-          ul: ['listbox', 'menu', 'menubar', 'radiogroup', 'tablist', 'tree', 'treegrid'],
-          ol: ['listbox', 'menu', 'menubar', 'radiogroup', 'tablist', 'tree', 'treegrid'],
-          li: ['menuitem', 'option', 'row', 'tab', 'treeitem'],
-          table: ['grid'],
-          td: ['gridcell'],
-        },
-      ],
-      'jsx-a11y/no-noninteractive-tabindex': ['error', { roles: ['tabpanel'] }],
-      'jsx-a11y/no-redundant-roles': ['error'],
-      // 'jsx-a11y/no-static-element-interactions': [
-      //   'error',
-      //   { handlers: ['onClick', 'onMouseDown', 'onMouseUp', 'onKeyPress', 'onKeyDown', 'onKeyUp'] },
-      // ],
-      'jsx-a11y/prefer-tag-over-role': ['error'],
-      'jsx-a11y/role-has-required-aria-props': ['error'],
-      'jsx-a11y/role-supports-aria-props': ['error'],
-      'jsx-a11y/scope': ['error'],
-      'jsx-a11y/tabindex-no-positive': ['error'],
-
       // @typescript-eslint
       '@typescript-eslint/method-signature-style': ['error'],
       '@typescript-eslint/no-array-constructor': ['error'],
