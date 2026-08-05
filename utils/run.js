b210329d035ffd4827a4932e6423d5bd29ef8fb8
zhrk <zshchipets@gmail.com>
2026-08-05 15:04:03 +0400

    fix run fn exit code

--- utils/run.js
+++ utils/run.js
@@ -1,5 +1,11 @@
 const { spawnSync } = require('node:child_process');
 
-const run = (command, args) => spawnSync(command, args, { shell: true, stdio: 'inherit' });
+const run = (command, args) => {
+  const result = spawnSync(command, args, { shell: true, stdio: 'inherit' });
+
+  process.exitCode = result.status ?? 1;
+
+  return result;
+};
 
 module.exports = run;
